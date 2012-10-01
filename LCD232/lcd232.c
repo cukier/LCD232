dc0f5780f00c12de6560fe3d68137fae642c1315
cuki <cuki@192.168.0.37>
2012-10-01 18:55:59 -0300

    f

--- LCD232/lcd232.c
+++ LCD232/lcd232.c
@@ -1,4 +1,4 @@
-                                 /*
+/*
  * lcd232.c
  *
  *  Created on: 01/10/2012
@@ -12,22 +12,22 @@
 
 #include"lcd_4b.c"
 
-int main(void){
+int main(void) {
 //   lcd_init();
 //   delay_ms(300);
-   while(true){
-      output_toggle(pin_a0);
-      output_toggle(pin_a1);
-      output_toggle(pin_a7);
-      output_toggle(pin_a6);
-      output_toggle(pin_b5);
-      output_toggle(pin_b4);
+	while (true) {
+		output_toggle(pin_a0);
+		output_toggle(pin_a1);
+		output_toggle(pin_a7);
+//		output_toggle(pin_a6);
+		output_toggle(pin_b5);
+		output_toggle(pin_b4);
 //      delay_ms(500);
 //      printf(lcd, "\f");
 //      delay_ms(500);
 //      printf(lcd, "Hola");
-      delay_ms(100);
+		delay_ms(10);
 //      printf(lcd, "\nMundo");
-   }
-   return 0;
+	}
+	return 0;
 }
