eeec220da44d5cda4c8a6a167ebea1fbf5a055aa
cuki <cuki@192.168.0.37>
2012-10-01 18:21:27 -0300

    commit

---
+++ LCD232/lcd232.c
@@ -0,0 +1,17 @@
+/*
+ * lcd232.c
+ *
+ *  Created on: 01/10/2012
+ *      Author: cuki
+ */
+
+#include<16F628A.h>
+
+#fuses NOWDT,NOPUT,INTRC,NOMCLR,NOBROWNOUT,NOLVP,NOCPD,NOPROTECT
+#use delay(clock=4MHz)
+
+#include"lcd_4b.c"
+
+int main(void){
+
+}
