d9083b862a2f997fb849fe1174052193b0646f01
cuki <mauricio@krebs.com.br>
2013-10-03 14:37:10 -0300

    aumento tempo espera

--- lcd232.c
+++ lcd232.c
@@ -21,44 +21,44 @@ int line = 0;
 
 #INT_TIMER1
 void isr_timer1() {
-	clear_interrupt(INT_TIMER1);
-	setup_timer_1(T1_DISABLED);
-	write = 1;
+   clear_interrupt(INT_TIMER1);
+   setup_timer_1(T1_DISABLED);
+   write = 1;
 }
 
 #int_rda
 void serial_isr() {
-	clear_interrupt(INT_RDA);
-	if (line < 31) {
-		buffer[line++] = getc();
-		buffer[line] = '\0';
-	} else {
-		strcpy(buffer, "");
-		line = 0;
-	}
-	set_timer1(0);
-	setup_timer_1(T1_INTERNAL | T1_DIV_BY_1);
+   clear_interrupt(INT_RDA);
+   if (line < 31) {
+      buffer[line++] = getc();
+      buffer[line] = '\0';
+   } else {
+      strcpy(buffer, "");
+      line = 0;
+   }
+   set_timer1(0);
+   setup_timer_1(T1_INTERNAL | T1_DIV_BY_1);
 }
 
 int main(void) {
 
-	output_low(PIN_B3);
-	strcpy(buffer, "Done2");
-	lcd_init();
-	delay_ms(15);
+   output_low(PIN_B3);
+   strcpy(buffer, "Done2");
+   lcd_init();
+   delay_ms(150);
 
-	clear_interrupt(INT_TIMER1);
-	enable_interrupts(INT_RDA);
-	enable_interrupts(INT_TIMER1);
-	enable_interrupts(GLOBAL);
+   clear_interrupt(INT_TIMER1);
+   enable_interrupts(INT_RDA);
+   enable_interrupts(INT_TIMER1);
+   enable_interrupts(GLOBAL);
 
-	while (TRUE) {
-		if (write) {
-			printf(lcd, "\f%s", buffer);
-			buffer[0] = '\0';
-			line = 0;
-			write = 0;
-		}
-	}
-	return 0;
+   while (TRUE) {
+      if (write) {
+         printf(lcd, "\f%s", buffer);
+         buffer[0] = '\0';
+         line = 0;
+         write = 0;
+      }
+   }
+   return 0;
 }
