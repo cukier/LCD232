312dbc3e739cde9575ebcd8f753eda58b3190315
cuki <mauricio@krebs.com.br>
2015-02-26 17:26:22 -0300

    sem interrucao

--- lcd232.c
+++ lcd232.c
@@ -14,51 +14,14 @@
 
 #include "lcd_4b.c"
 
-#define max 40
-#define T1_ON T1_INTERNAL|T1_DIV_BY_1
-
-short write = 1;
-int buffer[max];
-int line = 0;
-
-#INT_TIMER1
-void isr_timer1() {
-	clear_interrupt(INT_TIMER1);
-	setup_timer_1(T1_DISABLED);
-	write = 1;
-}
-
-#int_rda
-void serial_isr() {
-	clear_interrupt(INT_RDA);
-	buffer[line++] = getc();
-	buffer[line] = '\0';
-	set_timer1(0);
-	setup_timer_1(T1_ON);
-}
-
 int main(void) {
 
-	strcpy(buffer, "Done");
-	write = TRUE;
 	lcd_init();
 	delay_ms(500);
-
-	clear_interrupt(INT_TIMER1);
-	clear_interrupt(INT_RDA);
-	enable_interrupts(INT_RDA);
-	enable_interrupts(INT_TIMER1);
-	enable_interrupts(GLOBAL);
+	printf(lcd, "\fDone");
 
 	while (TRUE) {
-		if (write) {
-			write = 0;
-			printf(lcd, "%s", buffer);
-			printf("%s", buffer);
-			for (line = 0; line < max; line++)
-				buffer[line] = '\0';
-			line = 0;
-		}
+		printf(lcd, "%c", getc());
 	}
 	return 0;
 }
